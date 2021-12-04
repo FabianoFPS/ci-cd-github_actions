908a37e1e935edfb119c9d3a09c85b08ea5cef3e
Fabiano F. P. Stoffel <ffpthai@gmail.com>
2021-12-04 15:58:42 -0300

    fabiano

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 
 const port = process.env.PORT || 8000;
 const app = express();
+const user = process.env.USER || 'Not found';
 
-app.get('/', (request, response) => response.send('Hello word!'));
+app.get('/', (request, response) => response.send(`Hello word ${user}!`));
 app.listen(port, () => console.log(`Our app is up and running on port ${port}!`));
 
 module.exports = {
