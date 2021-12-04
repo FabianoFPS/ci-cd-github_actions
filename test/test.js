502466fc4af26e8de129c8318f200f5ef9db20d3
Fabiano F. P. Stoffel <ffpthai@gmail.com>
2021-12-04 16:05:46 -0300

    test

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ const { app } = require('../index');
 
 describe('GET /', () => {
   it('responds with Hello word', (done) => {
-    request(app).get('/').expect('Hello word!', done)
+    const user = process.env.USER;
+    request(app).get('/').expect(`Hello word ${user}!`, done)
   })
 });
