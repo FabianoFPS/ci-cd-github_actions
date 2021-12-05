54b011bd8efd4e6deec64787e2c5aa29e76a3ea2
Fabiano F. P. Stoffel <ffpthai@gmail.com>
2021-12-05 14:30:42 -0300

    Heroku var

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ const { app } = require('../index');
 
 describe('GET /', () => {
   it('responds with Hello word', (done) => {
-    const user = process.env.USER;
+    const user = process.env.HEROKU_VAR_VALUE || 'Not found';
     request(app).get('/').expect(`Hello word ${user}!`, done)
   })
 });
